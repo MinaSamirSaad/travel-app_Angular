edcf5e4d2a5987aeea40abc127a1a9a91318e1a5
ahmedakram14 <ahmedelkhergawy98@gmail.com>
2024-02-24 18:02:42 +0200

    Exoitic places components in home page added + changes in global css + update config in json style between css and bootstrap

---
+++ src/app/components/home/exotic-places/exotic-places.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exotic-places',
+  standalone: true,
+  imports: [],
+  templateUrl: './exotic-places.component.html',
+  styleUrl: './exotic-places.component.css'
+})
+export class ExoticPlacesComponent {
+
+}
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
+import {
+  ExoticPlacesComponent,
+} from '../../components/home/exotic-places/exotic-places.component';
+
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [ExoticPlacesComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
