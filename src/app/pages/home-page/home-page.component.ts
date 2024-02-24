0c6d5bd06bd309ecd322df757673a0905412de58
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-02-25 00:04:14 +0200

    update the main

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,9 @@
-import { CarouselComponent } from "../../components/home/carousel/carousel.component";
-import { HeaderComponent } from "./../../components/home/header/header.component";
 import { Component } from "@angular/core";
 
 @Component({
   selector: "app-home-page",
   standalone: true,
-  imports: [HeaderComponent, CarouselComponent],
+  imports: [],
   templateUrl: "./home-page.component.html",
   styleUrl: "./home-page.component.css",
 })
