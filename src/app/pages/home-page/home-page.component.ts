f432708e472b7584a14721c297fdc3f62717a31c
ahmedakram14 <ahmedelkhergawy98@gmail.com>
2024-02-24 23:08:22 +0200

    updated why choose us and exotic

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -3,11 +3,14 @@ import { Component } from '@angular/core';
 import {
   ExoticPlacesComponent,
 } from '../../components/home/exotic-places/exotic-places.component';
+import {
+  WhyChooseUsComponent,
+} from '../../components/home/why-choose-us/why-choose-us.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [ExoticPlacesComponent],
+  imports: [ExoticPlacesComponent , WhyChooseUsComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
