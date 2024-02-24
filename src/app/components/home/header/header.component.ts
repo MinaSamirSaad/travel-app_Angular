4218c830f86459c6c351f617d095ab9ce40a85d0
Mohamed Mounir <116511682+mohamedmounir2425@users.noreply.github.com>
2024-02-24 22:41:59 +0200

    v.2-carousel

--- src/app/components/home/header/header.component.ts
+++ src/app/components/home/header/header.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component, HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
+  selector: "app-header",
   standalone: true,
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  imports: [CommonModule],
+  templateUrl: "./header.component.html",
+  styleUrl: "./header.component.css",
 })
 export class HeaderComponent {
+  isScrolled: boolean = false;
 
+  @HostListener("window:scroll", [])
+  onScroll(): void {
+    this.isScrolled = window.scrollY > 300;
+  }
 }
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
+import { CarouselComponent } from "../../components/home/carousel/carousel.component";
 import { HeaderComponent } from "./../../components/home/header/header.component";
 import { Component } from "@angular/core";
 
 @Component({
   selector: "app-home-page",
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CarouselComponent],
   templateUrl: "./home-page.component.html",
   styleUrl: "./home-page.component.css",
 })
