55bd8451065ed5bedd7dc807f2d7368d631bb1b8
Sayed <sayedshehata33@gmail.com>
2024-02-25 13:49:23 +0200

    feat: :sparkles: Generate Slider Component

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -10,11 +10,12 @@ import { HeaderComponent } from '../../components/home/header/header.component';
 import {
   WhyChooseUsComponent,
 } from '../../components/home/why-choose-us/why-choose-us.component';
+import { SliderComponent } from '../../components/home/slider/slider.component';
 
 @Component({
   selector: "app-home-page",
   standalone: true,
-  imports: [HeaderComponent, CarouselComponent , ExoticPlacesComponent , WhyChooseUsComponent],
+  imports: [HeaderComponent, CarouselComponent , ExoticPlacesComponent , WhyChooseUsComponent , SliderComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
