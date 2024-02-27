05e0ddd58c499460b38dc91609e705ccbd2fe615
Sayed <sayedshehata33@gmail.com>
2024-02-27 16:17:58 +0200

    feat: :alien: Add google auth

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+declare var google: any;
+
+import { Component, OnInit, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +12,39 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+ private router=inject(Router);
+//google intialize
+ngOnInit():void{
+  google.accounts.id.initialize({
+    client_id: '74021032483-lm9ummnk7o7kovemrlkd826ul63u83hd.apps.googleusercontent.com',
+    callback: (resp:any)=>this.handleLogin(resp)
+  });
+  google.accounts.id.renderButton(document.getElementById("google-btn"),{
+    // theme: 'filled_blue',
+    size: 'large',
+    shape: 'rectangle',
+    text: 'continue with google',
+    width:300,
+  })
+}
+
+// decode token
+private decodeToken(token:string){
+  return JSON.parse(atob(token.split(".")[1]));
+}
+
+// handling google login 
+handleLogin(respone:any){
+  if(respone){
+    //deconde the token
+    const payload = this.decodeToken(respone.credential);
+    //store in session
+    sessionStorage.setItem("loggedInUser",JSON.stringify(payload));
+    //navigate to home
+    this.router.navigate(['/home']);
+  }
+}
 
 
 // form validation
