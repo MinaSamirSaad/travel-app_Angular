e0d7771f8b39b26ded803943be92d90c10b1ca54
Mohamed Mounir <116511682+mohamedmounir2425@users.noreply.github.com>
2024-02-27 09:09:22 +0200

    fix bug in home-pages-ts

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,23 +1,22 @@
 import { Component } from "@angular/core";
 
-import {
-  CarouselComponent,
-} from '../../components/home/carousel/carousel.component';
-import {
-  ExoticPlacesComponent,
-} from '../../components/home/exotic-places/exotic-places.component';
-import { HeaderComponent } from '../../components/home/header/header.component';
-import {
-  WhyChooseUsComponent,
-} from '../../components/home/why-choose-us/why-choose-us.component';
-import { SliderComponent } from '../../components/home/slider/slider.component';
+import { CarouselComponent } from "../../components/home/carousel/carousel.component";
+import { ExoticPlacesComponent } from "../../components/home/exotic-places/exotic-places.component";
+import { HeaderComponent } from "../../components/home/header/header.component";
+import { WhyChooseUsComponent } from "../../components/home/why-choose-us/why-choose-us.component";
+import { SliderComponent } from "../../components/home/slider/slider.component";
 
 @Component({
   selector: "app-home-page",
   standalone: true,
- "./home-page.component.css",
-  imports: [HeaderComponent, CarouselComponent , ExoticPlacesComponent , WhyChooseUsComponent , SliderComponent],
-  templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  imports: [
+    HeaderComponent,
+    CarouselComponent,
+    ExoticPlacesComponent,
+    WhyChooseUsComponent,
+    SliderComponent,
+  ],
+  templateUrl: "./home-page.component.html",
+  styleUrl: "./home-page.component.css",
 })
 export class HomePageComponent {}
