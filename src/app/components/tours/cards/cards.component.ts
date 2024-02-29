699b44b4eeca1b20053c71c1704c53cd5acdacfd
Mohamed Mounir <116511682+mohamedmounir2425@users.noreply.github.com>
2024-02-29 09:34:40 +0200

    update tours cards

--- src/app/components/tours/cards/cards.component.ts
+++ src/app/components/tours/cards/cards.component.ts
@@ -1,23 +1,61 @@
-import { CommonModule } from '@angular/common';
-import {
-  Component,
-  ViewEncapsulation,
-} from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from "@angular/common";
+import { Component, ViewEncapsulation } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { ButtonModule } from 'primeng/button';
-import { CardModule } from 'primeng/card';
-import { RatingModule } from 'primeng/rating';
+import { ButtonModule } from "primeng/button";
+import { RatingModule } from "primeng/rating";
+import { Subscription } from "rxjs";
+import { FilterService } from "../../../services/trips/filter.service";
+import { TripsService } from "../../../services/trips/trips.service";
+import { HttpClientModule } from "@angular/common/http";
+import { FilterPipe } from "../../../pipes/filter.pipe";
+import { CardComponent } from "../../card/card.component";
 
 @Component({
-  selector: 'app-cards',
+  selector: "app-cards",
   standalone: true,
-  imports: [CommonModule ,FormsModule , ButtonModule , CardModule , RatingModule],
-  templateUrl: './cards.component.html',
-  styleUrl: './cards.component.css',
+  imports: [
+    CommonModule,
+    FormsModule,
+    ButtonModule,
+    CardComponent,
+    RatingModule,
+    HttpClientModule,
+    FilterPipe,
+  ],
+  providers: [FilterService, TripsService],
+  templateUrl: "./cards.component.html",
+  styleUrl: "./cards.component.css",
   encapsulation: ViewEncapsulation.None,
 })
 export class CardsComponent {
+  // search functionality
+  trips: any;
+  searchTerm: string = "";
+  private subscription!: Subscription;
+  constructor(
+    private _FilterService: FilterService,
+    private _TripsService: TripsService
+  ) {
+    this.subscription = this._FilterService.search$.subscribe({
+      next: (term) => {
+        this.searchTerm = term;
+      },
+    });
+  }
+  ngOnInit(): void {
+    this._TripsService.getTrips().subscribe({
+      next: ({ data }) => {
+        this.trips = data;
+        console.log(data);
+      },
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+  //end search functionality
   value: number = 3;
   isHovered: boolean = false;
   isClicked: boolean = false;
