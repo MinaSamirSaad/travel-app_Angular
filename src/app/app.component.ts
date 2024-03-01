94f62d970a70bd2006b9613c5ded33b68325afb7
Mohamed Mounir <116511682+mohamedmounir2425@users.noreply.github.com>
2024-03-01 23:07:24 +0200

    routes in login & payment pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { Router, RouterOutlet } from "@angular/router";
 
-import {
-  FooterComponent,
-} from './components/app-footer2/footer/footer.component';
-import { HeaderComponent } from './components/home/header/header.component';
-import { HotelsComponent } from './components/hotels/hotels.component';
-import { OfferComponent } from './components/offer/offer.component';
-import { ReviewsComponent } from './components/reviews/reviews.component';
-import {
-  CountriesComponent,
-} from './pages/countries-page/countries/countries.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { RegisterComponent } from './components/register/register.component';
-import { RegisterPageComponent } from './pages/register-page/register-page.component';
-import { PayPageComponent } from './pages/pay-page/pay-page.component';
+import { FooterComponent } from "./components/app-footer2/footer/footer.component";
+import { HeaderComponent } from "./components/home/header/header.component";
+import { HotelsComponent } from "./components/hotels/hotels.component";
+import { OfferComponent } from "./components/offer/offer.component";
+import { ReviewsComponent } from "./components/reviews/reviews.component";
+import { CountriesComponent } from "./pages/countries-page/countries/countries.component";
+import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { RegisterPageComponent } from "./pages/register-page/register-page.component";
+import { PayPageComponent } from "./pages/pay-page/pay-page.component";
+import { AuthLayoutComponent } from "./layout/auth-layout/auth-layout.component";
+import { SystemLayoutComponent } from "./layout/system-layout/system-layout.component";
 
 @Component({
   selector: "app-root",
@@ -29,12 +27,21 @@ import { PayPageComponent } from './pages/pay-page/pay-page.component';
     HotelsComponent,
     CountriesComponent,
     RegisterPageComponent,
-    PayPageComponent
-
+    PayPageComponent,
+    AuthLayoutComponent,
+    SystemLayoutComponent,
   ],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
 export class AppComponent {
   title = "travel-app_Angular";
+  constructor(private router: Router) {}
+  isLoginOrRegisterRoute(): boolean {
+    return (
+      this.router.url.includes("login") ||
+      this.router.url.includes("register") ||
+      this.router.url.includes("pay")
+    );
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,19 +7,32 @@ import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { ToursPageComponent } from "./pages/tours-page/tours-page/tours-page.component";
 import { TripDetailsComponent } from "./components/trip-details/trip-details.component";
 import { FavoriteTripsComponent } from "./pages/favorite-trips/favorite-trips.component";
-import { RegisterPageComponent } from './pages/register-page/register-page.component';
-import { PayPageComponent } from './pages/pay-page/pay-page.component';
+import { RegisterPageComponent } from "./pages/register-page/register-page.component";
+import { PayPageComponent } from "./pages/pay-page/pay-page.component";
+import { SystemLayoutComponent } from "./layout/system-layout/system-layout.component";
+import { AuthLayoutComponent } from "./layout/auth-layout/auth-layout.component";
 
 export const routes: Routes = [
-  { path: "", redirectTo: "home", pathMatch: "full" },
-  { path: "home", component: HomePageComponent },
-  { path: "tours", component: ToursPageComponent },
-  { path: "offer", component: OfferComponent },
-  { path: "countries", component: CountriesComponent },
-  { path: "trip/:id", component: TripDetailsComponent },
-  { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterPageComponent },
-  { path: "pay", component: PayPageComponent },
-  { path: "home", component: HomePageComponent },
-  { path: "favourite", component: FavoriteTripsComponent },
+  {
+    path: "",
+    component: SystemLayoutComponent,
+    children: [
+      { path: "", redirectTo: "home", pathMatch: "full" },
+      { path: "home", component: HomePageComponent },
+      { path: "tours", component: ToursPageComponent },
+      { path: "offer", component: OfferComponent },
+      { path: "countries", component: CountriesComponent },
+      { path: "trip/:id", component: TripDetailsComponent },
+      { path: "favorite", component: FavoriteTripsComponent },
+    ],
+  },
+  {
+    path: "",
+    component: AuthLayoutComponent,
+    children: [
+      { path: "login", component: LoginComponent },
+      { path: "register", component: RegisterPageComponent },
+      { path: "pay", component: PayPageComponent },
+    ],
+  },
 ];
