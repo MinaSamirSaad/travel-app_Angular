47a618d26ac8b53926c6dec3deed347261516460
ahmedakram14 <ahmedelkhergawy98@gmail.com>
2024-03-02 19:34:21 +0200

    Add countries page

--- src/app/pages/countries-page/countries/countries.component.ts
+++ src/app/pages/countries-page/countries/countries.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
+
+import {
+  CountriesService,
+} from '../../../services/countries/countries.service';
+import { HotelsService } from '../../../services/hotels/hotels.service';
 
 @Component({
   selector: 'app-countries',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
+  providers : [CountriesService , HotelsService],
   templateUrl: './countries.component.html',
-  styleUrl: './countries.component.css'
+  styleUrl: './countries.component.css',
+
 })
-export class CountriesComponent {
+export class CountriesComponent implements OnInit{
+  constructor(private countriesService : CountriesService , private hotelsService : HotelsService){}
+  countriesData : any;
+  countries: any ;
+  countrydiscription : any;
+  ngOnInit(): void {
+    this.countriesService.getCountries().subscribe(
+      {next :  (data) =>{this.countriesData = data
+      this.countries = this.countriesData.data
+      }
+      }
+    );
+    this.hotelsService.getHotels();
+  }
 
 }
