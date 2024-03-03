c3ccd99ac9a7f8512d4754aa83d2e207632e4419
Mohamed Mounir <116511682+mohamedmounir2425@users.noreply.github.com>
2024-03-03 21:05:44 +0200

    update navbar_hidden-spinner-navbar_responsive

--- src/app/components/home/header/header.component.ts
+++ src/app/components/home/header/header.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from "@angular/common";
 import { Component, HostListener } from "@angular/core";
 import {
+  ActivatedRoute,
   IsActiveMatchOptions,
   NavigationEnd,
   Router,
@@ -17,10 +18,19 @@ import {
 export class HeaderComponent {
   isScrolled: boolean = false;
   isFavouriteActive: boolean = false;
-  constructor(private router: Router) {}
+  bgNavbar: boolean = false;
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
   @HostListener("window:scroll", [])
   onScroll(): void {
-    this.isScrolled = window.scrollY > 300;
+    // this.isScrolled = window.scrollY > 300;
+    if (this.activatedRoute.firstChild?.snapshot.data["bgNav"]) {
+      this.isScrolled = true;
+    } else {
+      this.isScrolled = window.scrollY > 300;
+    }
+    // this.isScrolled = this.activatedRoute.firstChild?.snapshot.data["bgNav"]
+    //   ? window.scrollY > 0
+    //   : window.scrollY > 300;
   }
 
   ngOnInit() {
@@ -32,7 +42,10 @@ export class HeaderComponent {
           fragment: "ignored",
           matrixParams: "ignored",
         };
+        this.isScrolled =
+          this.activatedRoute.firstChild?.snapshot.data["bgNav"];
         this.isFavouriteActive = this.router.isActive("favourite", options);
+        this.bgNavbar = !this.activatedRoute.firstChild?.snapshot.data["bgNav"];
       }
     });
   }
