44f99e195fc33599cb42fbaa287d5cf1da3d50d3
ahmedakram14 <ahmedelkhergawy98@gmail.com>
2024-03-06 18:29:43 +0200

    update trip details

--- src/app/components/trip-details/trip-details.component.ts
+++ src/app/components/trip-details/trip-details.component.ts
@@ -1,11 +1,21 @@
-import { Component, ViewEncapsulation, inject } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { RatingModule } from "primeng/rating";
-import { CommonModule } from "@angular/common";
-import { DropdownModule } from "primeng/dropdown";
-import { ActivatedRoute, Router } from "@angular/router";
-import { TripsService } from "../../services/trips/trips.service";
-import { HttpClientModule } from "@angular/common/http";
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import {
+  Component,
+  inject,
+  ViewEncapsulation,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  ActivatedRoute,
+  Router,
+} from '@angular/router';
+
+import { DropdownModule } from 'primeng/dropdown';
+import { RatingModule } from 'primeng/rating';
+
+import { HotelsService } from '../../services/hotels/hotels.service';
+import { TripsService } from '../../services/trips/trips.service';
 
 @Component({
   selector: "app-trip-details",
@@ -17,7 +27,7 @@ import { HttpClientModule } from "@angular/common/http";
     FormsModule,
     DropdownModule,
   ],
-  providers: [TripsService],
+  providers: [TripsService , HotelsService ],
   templateUrl: "./trip-details.component.html",
   styleUrl: "./trip-details.component.css",
   encapsulation: ViewEncapsulation.None,
@@ -26,37 +36,53 @@ export class TripDetailsComponent {
   private router = inject(Router);
   constructor(
     private route: ActivatedRoute,
-    private _TripsService: TripsService
+    private _TripsService: TripsService,
+    private _HotelsService: HotelsService
   ) {}
   id: any;
   trip: any;
   value = 2;
   cities: any[] | undefined;
-
+  hotelID : any;
+  hotel : any;
   selectedCity: any | undefined;
+  hotelData : any;  ngOnInit() {
 
-  ngOnInit() {
-    this.cities = [
-      { name: "Domestic", code: "domestic" },
-      { name: "Outgoing", code: "outgoing" },
-      { name: "Honeymoon", code: "honeymoon" },
-      { name: "Nile cruise", code: "nileCruise" },
-    ];
 
     this.route.params.subscribe({
       next: (params) => {
         this.id = params["id"];
       },
     });
+
     this._TripsService.getTripById(this.id).subscribe({
       next: ({ data }) => {
         this.trip = data.trip;
+        this.hotelID = data.trip.hotel.id
+        console.log(this.hotelID)
+
+        this._HotelsService.getHotel(this.hotelID).subscribe({
+          next: (data) => {
+            this.hotel = data
+            this.hotelData = this.hotel.data.hotel
+            console.log(this.hotelData)
+          }
+        })
+
+
       },
     });
+
   }
   goToBookingPage(id: any) {
     this.router.navigate([`pay/${id}`]).then(() => {
       window.location.reload();
     });
   }
+
+
+  changeHotelImg(img : any){
+    this.trip.imgUrl = img;
+  }
+
 }
