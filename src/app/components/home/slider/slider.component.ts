02eff152c86f4b9358f8d05912841a68274f7e6f
Sayed <sayedshehata33@gmail.com>
2024-03-08 11:59:02 +0200

    feat: :sparkles: Add hotels routing in homePage

--- src/app/components/home/slider/slider.component.ts
+++ src/app/components/home/slider/slider.component.ts
@@ -30,7 +30,6 @@ export class SliderComponent implements OnInit {
   ngOnInit(): void {
     this.trips.getTrips().subscribe((data) => {
       this.trip = data.data.slice(0, 12);
-      console.log(this.trip);
     });
   }
 
