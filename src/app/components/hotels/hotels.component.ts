201045d87af8c255c809cdb73047ca8b402c74c3
ahmedakram14 <ahmedelkhergawy98@gmail.com>
2024-03-09 14:22:41 +0200

    update hotel component

--- src/app/components/hotels/hotels.component.ts
+++ src/app/components/hotels/hotels.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-import { HotelsService } from '../../services/hotels/hotels.service';
-import { RouterModule } from '@angular/router';
-import { TripsService } from '../../services/trips/trips.service';
+import {
+  Router,
+  RouterModule,
+} from '@angular/router';
 
+import { HotelsService } from '../../services/hotels/hotels.service';
+import { TripsService } from '../../services/trips/trips.service';
 
 @Component({
   selector: 'app-hotels',
@@ -13,14 +16,29 @@ import { TripsService } from '../../services/trips/trips.service';
   styleUrl: './hotels.component.css'
 })
 export class HotelsComponent {
-constructor(private hotelsService: HotelsService , private trips:TripsService) { }
+constructor(private hotelsService: HotelsService , private trips:TripsService , private router : Router) { }
 
 hotels: any = [];
+popularHotels: any = [];
+hotelID: any ;
+trip : any = [];
+ check = (id : any) => {
+   this.hotelID = id ;
+   console.log(this.hotelID)
+   this.trips.getTrips().subscribe((data : any)=> {
+     this.trip = data.data
+     let filtered = this.trip.filter((tri : any) => {
+       return tri.hotel.id === this.hotelID
+     })
+     this.router.navigate([`/trip/${filtered[0]._id}`])
+   })
+}
 
 ngOnInit() {
   this.hotelsService.getHotels().subscribe((data:any) => {
-    this.hotels = data.data.slice(0, 4);
-
+    this.hotels = data.data
+    console.log(this.hotels)
+    this.popularHotels = this.hotels.filter((hotel : any)=> hotel.hotelRate === 5).slice(2,6)
   });
 
 }
